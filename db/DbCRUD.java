c09278777aa4a64a5a2513396b8e9fc99bba079c
leekyuyoung20231115 <150870358+leekyuyoung20231115@users.noreply.github.com>
2023-11-28 12:13:06 +0900

    Add files via upload

--- db/DbCRUD.java
+++ db/DbCRUD.java
@@ -3,8 +3,9 @@ package db;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 
-public class DbCRUD {
+public class DbCRUD implements AutoCloseable{
 	private static Connection conn = null;	
+	private static PreparedStatement pstmt = null;
 	static {
 		try {
 			conn = DbConnection.getConnection();
@@ -15,8 +16,6 @@ public class DbCRUD {
 	}
 	public static void insertData(String pname,int stock, int price) throws Exception {
 		// db에 데이터 추가
-		Connection conn=null;
-		PreparedStatement pstmt = null;		
 		conn = DbConnection.getConnection();
 		String sql = "insert into product(pname,stock,price) values(?,?,?)";
 		pstmt= conn.prepareStatement(sql);
@@ -24,6 +23,14 @@ public class DbCRUD {
 		pstmt.setInt(2, stock);
 		pstmt.setInt(3, price);
 		pstmt.executeUpdate();
-//		DbConnection.close(pstmt);DbConnection.close(conn);		
 	}
+	
+	
+	
+	@Override
+	public void close() throws Exception {
+		pstmt.close();
+		conn.close();		
+	}
+	
 }
