b123707565d06da0b4208d59a58734e9fc014de6
leekyuyoung20231115 <150870358+leekyuyoung20231115@users.noreply.github.com>
2023-11-28 16:48:33 +0900

    Add files via upload

--- JavaFinalProject/src/product/Demo.java
+++ JavaFinalProject/src/product/Demo.java
@@ -3,6 +3,7 @@ package product;
 import java.sql.Date;
 import java.sql.ResultSet;
 import java.text.SimpleDateFormat;
+import java.util.List;
 import java.util.Scanner;
 
 import db.DbCRUD;
@@ -10,52 +11,52 @@ import db.DbCRUD;
 public class Demo {
 
 	public static void main(String[] args) throws Exception {
-		System.out.println("1. 재고목록\t\t2. 입고/출고");
-		System.out.println("3. 로그인/로그아웃\t\t\t4. 종료");
-		System.out.print("input menu : ");
-		Scanner sc = new Scanner(System.in);
-		int menuNum = sc.nextInt();
-		ResultSet rs = null;
-		switch (menuNum) {
-		case 1:		
-			rs =  DbCRUD.selectByName("",true);
-			while (rs.next()) {
-				String pname = rs.getString("pname");
-				int stock = rs.getInt("stock");
-				int price = rs.getInt("price");
-				Date createDt =  rs.getDate("create_dt");
-				Date modifyDt =  rs.getDate("modify_dt");
+		int menuNum = 0;
+		while(menuNum != 4) {
+			System.out.println("1. 재고목록\t\t2. 입고/출고");
+			System.out.println("3. 로그인/로그아웃\t\t4. 종료");
+			System.out.print("input menu : ");
+			Scanner sc = new Scanner(System.in);
+			menuNum = sc.nextInt();
+			ResultSet rs = null;
+			switch (menuNum) {
+			case 1:		
+				List<ProductVO> results =  DbCRUD.selectByName("",true);
 				SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+				for (ProductVO vo : results) {
+					System.out.format("%s  %d  %d  %s  %s\n", 
+							vo.getPname(),vo.getStock(),vo.getPrice(),
+							sdf.format(vo.getCreate_dt()),
+							sdf.format(vo.getModify_dt())
+							);					
+				}
 				
-				System.out.format("%s  %d  %d  %s  %s\n", 
-						pname,stock,price,
-						sdf.format(createDt),
-						sdf.format(modifyDt)
-						);
+				break;
+			case 2:
+				System.out.print("제품명을 입력하세요:");
+				String pname = sc.next().trim();
+				System.out.print("수량을 입력하세요");
+				int stock = sc.nextInt();
+				System.out.print("가격을 입력하세요");
+				int price = sc.nextInt();
+				
+				List<ProductVO> resultOne =  DbCRUD.selectByName(pname,false);
+				if(resultOne.size() > 0) {  // 제품이 있음				
+					DbCRUD.updataData(pname, stock, price, rs.getInt("id"));
+					System.out.println("수정했습니다.");
+				}else {  // 신규상품임
+					DbCRUD.insertData(pname, stock, price);
+					System.out.println("추가했습니다.");
+				}				
+				break;			
+			case 3:
+				break;
+			case 4: break;
+			default:
+				System.out.println("잘못 입력했습니다.");
+				break;
 			}
-			break;
-		case 2:
-			System.out.print("제품명을 입력하세요:");
-			String pname = sc.next().trim();
-			System.out.print("수량을 입력하세요");
-			int stock = sc.nextInt();
-			System.out.print("가격을 입력하세요");
-			int price = sc.nextInt();
-			
-			rs =  DbCRUD.selectByName(pname,false);
-			if(rs.next()) {  // 제품이 있음				
-				DbCRUD.updataData(pname, stock, price, rs.getInt("id"));
-				System.out.println("수정했습니다.");
-			}else {  // 신규상품임
-				DbCRUD.insertData(pname, stock, price);
-				System.out.println("추가했습니다.");
-			}
-			
-			break;
-
-		default:
-			break;
-		}
+		} // end while
 	}
 
 }
